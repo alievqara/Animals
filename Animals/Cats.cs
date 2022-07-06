6924f41207951ea8c502ac8e0df1a84e875e7dac
Aliev Qara <alievqara@outlook.com>
2022-07-06 23:16:09 +0400

    Animals

---
+++ Animals/Cats.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Cats
+    {
+        public string name;
+        public string color;
+        public int age;
+        public string type;
+
+        public Cats(string name, string color, int age, string type)
+        {
+            this.name = name;
+            this.color = color;
+            this.age = age;
+            this.type = type;
+        }
+    }
+}
---
+++ Animals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Animals
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+
+            Dogs dog = new Dogs("Dobby", "Black", 5, "Pitbull");
+
+            Console.WriteLine($"{dog.name},{dog.color},{dog.age},{dog.type}");
+
+            Cats cat = new Cats("Knopka", "Black", 4, "Bombay");
+
+            Console.WriteLine($"{cat.name},{cat.color},{cat.age},{cat.type}");
+
+
+        }
+
+    }
+}
